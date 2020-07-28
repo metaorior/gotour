4f236463c55426617d3edae878660a107da6f1ae
metaorior <metaorior@gmail.com>
2020-07-28 01:08:35 -0300

    always learn

--- functions/multiple_results.go
+++ functions/multiple_results.go
@@ -1,27 +1,33 @@
 package main
 
 import (
-"strconv"
+//"strconv"
 	"fmt"
 )
 
-func swap(x, y string) (string, string) {
-	return y, x
-}
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
 
 func main() {
-	a, b := swap("Hello", "world")
-	fmt.Println(a,b)
+	//a, b := swap("Hello", "world")
+	//fmt.Println(a,b)
 
 
-	num1, num2, num3 := crazy(700, 200, 10000)
-	fmt.Println(num1, num2, num3)
+	//num1, num2, num3 := crazy(700, 200, 10000)
+	//fmt.Println(num1, num2, num3)
+	total := careacu(500,1000)
+	fmt.Println(total)
 }
 
-func crazy(a,b,c int) (string, string, string) {
-  convert := strconv.Itoa
-	return (convert(b) + convert(c) + convert(a)),(convert(b) + convert(c) + convert(a)),(convert(b) + convert(c) + convert(a))
-}
+// func crazy(a,b,c int) (string, string, string) {
+//   convert := strconv.Itoa
+// 	return (convert(b) + convert(c) + convert(a)),(convert(b) + convert(c) + convert(a)),(convert(b) + convert(c) + convert(a))
+// }
 
+func careacu(caio,sarah int) (int) {	
+	return caio + sarah
+
+}
 // func crazyInverter3(john, car, trash, mouse, m90 double) ()
 //
---
+++ functions/pointers.go
@@ -0,0 +1,23 @@
+package main 
+
+
+import (
+	"fmt"
+)
+
+func main() {
+	i, j := 42, 2701
+
+	// tell that p value is equal to i value
+	p := &i 
+	fmt.Println("the value of p is ", *p) // read i through the pointer
+	*p = 21 //set i throught the pointer
+	fmt.Println("the value of i is ", i) // see the new value of j
+
+	p = &j // p now is the value of j
+	fmt.Println("p = &j is " , j)
+	*p = *p / 37 //divide j through the pointer
+	fmt.Println("the value of j is ", j) // see the new value of j 
+
+
+ }
